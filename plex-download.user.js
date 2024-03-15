f06e7520961343aa5710c3035109c01a734151ca
taxicat1 <32942550+taxicat1@users.noreply.github.com>
2024-03-15 02:27:23 -0400

    Increment version

--- plex-download.user.js
+++ plex-download.user.js
@@ -2,7 +2,7 @@
 // @name         Plex downloader
 // @description  Adds a download button to the Plex desktop interface. Works on episodes, movies, whole seasons, and entire shows.
 // @author       Mow
-// @version      1.3.2
+// @version      1.3.3
 // @license      MIT
 // @grant        none
 // @match        https://app.plex.tv/desktop/*
